b289f7f46b1d45e6b70a65a6487764a2db889936
manderlai <manderlai87@gmai.com>
2016-09-06 15:25:31 +0300

    added charts

--- api/services/constants.js
+++ api/services/constants.js
@@ -1,7 +1,7 @@
 'use strict';
 var path = require('path');
 module.exports = Object.freeze({
-    LOCAL_UPLOADS_FOLDER_URL: 'api/uploads',
+    LOCAL_UPLOADS_FOLDER_URL: '/api/uploads',
     ABSOLUTE_UPLOADS_FOLDER_URL: path.join(__dirname, '../uploads'),
     PROJECT_ROOT_FOLDER: path.join(__dirname, '..', '..')
 });
