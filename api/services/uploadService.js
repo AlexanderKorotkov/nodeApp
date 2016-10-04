ca21158d7a3eac821256b07cc57c07b8ae6fa708
manderlai <manderlai87@gmai.com>
2016-10-04 11:54:03 +0300

    added charts

--- api/services/uploadService.js
+++ api/services/uploadService.js
@@ -7,35 +7,35 @@ var path = require('path');
 var fs = require('fs-extra');
 var shortid = require('shortid');
 var urljoin = require('url-join');
-var easyimg = require('easyimage');
+var thumb = require('node-thumbnail').thumb;
 
 exports.uploadImg = function(req) {
     var shortName = shortid.generate() + path.extname(req.files.file[0].name);
     var avatarPath = path.join(services.constants.ABSOLUTE_UPLOADS_FOLDER_URL, req.params.companyId, shortName);
     var avatarUrl = path.join(req.params.companyId, shortName);
-    var avatarThumbPath = path.join(services.constants.ABSOLUTE_UPLOADS_FOLDER_URL, req.params.companyId,'thumb', shortName);
+    var avatarThumbPath = path.join(services.constants.ABSOLUTE_UPLOADS_FOLDER_URL, req.params.companyId,'thumb');
     var avatarThumbUrl = path.join(req.params.companyId,'thumb', shortName);
     return new Promise(function(resolve/*, reject*/) {
         fs.move(req.files.file[0].path, avatarPath , function (err) {
             if (err) {
                 resolve(err)
             } else {
-                easyimg.resize({
-                    src: avatarPath, dst: avatarThumbPath,
-                    width:100, height:100
-                }).then(
-                    function() {
-                        resolve({
-                            imageUrl : urljoin(req.protocol + ':', req.headers.host, avatarUrl),
-                            imagePath: avatarPath,
-                            imageThumbUrl: urljoin(req.protocol + ':', req.headers.host, avatarThumbUrl),
-                            imageThumbPath: avatarThumbPath
-                        })
-                    },
-                    function (err) {
-                        throw err;
+                thumb({
+                    source: avatarPath,
+                    destination: avatarThumbPath,
+                    suffix: '',
+                    width: 100
+                }, function(err) {
+                    if (err){
+                        resolve(err);
                     }
-                );
+                    resolve({
+                        imageUrl : urljoin(req.protocol + ':', req.headers.host, avatarUrl),
+                        imagePath: avatarPath,
+                        imageThumbUrl: urljoin(req.protocol + ':', req.headers.host, avatarThumbUrl),
+                        imageThumbPath: path.join(avatarThumbPath,shortName)
+                    });
+                });
             }
         });
     })
