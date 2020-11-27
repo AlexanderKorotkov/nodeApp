1f036f7d442315248021c9638e4170abe74d11b9
alexander.korotkov <manderlai87@gmail.com>
2020-11-27 15:27:39 +0200

    update packages

--- api/conf.js
+++ api/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'ionicSimpleAppKey',
-    'database': 'mongodb+srv://manderlai:testapp@company-manager.qaouj.mongodb.net/company-manager?retryWrites=true&w=majority'
+    'database': 'mongodb+srv://manderlai:VBNMrf32167@company-manager.qaouj.mongodb.net/company-manager?retryWrites=true&w=majority'
 };
 // mongodb+srv://manderlai:<password>@company-manager.qaouj.mongodb.net/<dbname>?retryWrites=true&w=majority
-// 'mongodb://app:testapp@ds035965.mlab.com:35965/company-manager'
+// 'mongodb://app:testapp@ds035965.mlab.com:35965/company-manager'useNewUrlParser:
